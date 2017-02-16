aee4c7c3ea9ce264199bc438b133a9a02254b663
Chandu <Chandu@DESKTOP-PVGUP15>
2017-02-16 23:59:24 +0530

    second update

---
+++ AmazonSelTest/src/main/java/amazon/RegressionTesting.java
@@ -0,0 +1,32 @@
+package amazon;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class RegressionTesting 
+{
+   WebDriver driver;
+   public RegressionTesting(WebDriver d)
+   {
+	   driver=d;
+   }
+	
+   @Test
+   @Parameters({"browser"})
+	public void browser (String str) throws Exception
+	{
+		
+       if(str.matches("firefox"))
+       {
+       	System.setProperty("webdriver.gecko.driver","c://geckodriver.exe");
+			driver=new FirefoxDriver();
+			
+       }
+       Home h=new Home(driver);
+       h.checkout();
+
+	}
+  	
+}
---
+++ AmazonSelTest/src/main/java/amazon/Retest.java
@@ -0,0 +1,14 @@
+package amazon;
+
+import org.openqa.selenium.WebDriver;
+
+public class Retest 
+{
+	WebDriver driver;
+	   public Retest(WebDriver d)
+	   {
+		   driver=d;
+	   }
+
+	   
+}
